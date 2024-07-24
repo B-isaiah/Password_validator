81d1018759a7dbc25ff3c125f5f937573064e3fa
Blessing Isaiah <108679298+B-isaiah@users.noreply.github.com>
2024-07-24 17:22:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const validatorText = document.getElementById("validatorText");
+const ShowHide = document.getElementById("ShowHide");
+const passwordCheck = document.querySelectorAll(".password-check img");
+validatorText.focus();
+const combinations = [
+    { regex: /.{12}/, key: 0 },
+    { regex: /[A-Z]/, key: 1 },
+    { regex: /[a-z]/, key: 2 },
+    { regex: /[0-9]/, key: 3 },
+    { regex: /[^A-Za-z0-9]/, key: 4 },
+  ];
+
+  ShowHide.addEventListener("click", function (e) {
+    validatorText.type = validatorText.type === "text" ? "password" : "text";
+    e.target.setAttribute(
+      "src",
+      e.target.src.includes("hide.png") ? "show.png" : "hide.png"
+    );
+  });
+  validatorText.addEventListener("keyup", function (e) {
+    combinations.forEach((item) => {
+      const IsValid = item.regex.test(e.target.value);
+      const checkItem = passwordCheck[item.key];
+  
+      if (IsValid) {
+        checkItem.src = "checked.png";
+        checkItem.parentElement.style.color = "green";
+      } else {
+        checkItem.src = "close.png";
+        checkItem.parentElement.style.color = "red";
+      }
+    });
+  });
